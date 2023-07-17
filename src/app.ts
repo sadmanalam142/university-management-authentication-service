a325a7d03814957d6e722bd5970f1f14717dd560
sadmanalam142 <sadmanalam142@gmail.com>
2023-07-17 19:53:13 +0600

    [DONE] creating user service, controller, route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.router'
 const app: Application = express()
 
 app.use(cors())
@@ -7,6 +8,8 @@ app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/v1/users/', usersRouter.router)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('University Application Running')
--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -3,21 +3,26 @@ import { IUser } from './users.interface'
 
 type UserModel = Model<IUser, object>
 
-const userSchema = new Schema<IUser>({
-  id: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  role: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-})
+)
 
 export const User = model<IUser, UserModel>('User', userSchema)
