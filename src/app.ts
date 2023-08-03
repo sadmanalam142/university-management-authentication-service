ebd94d78b7e5b48c421efde68103da3619e91a21
sadmanalam142 <sadmanalam142@gmail.com>
2023-08-03 19:48:23 +0600

    [DONE] creating academic semesters

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import { UserRoutes } from './app/modules/user/user.router';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 const app: Application = express();
 
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users/', UserRoutes.router);
+app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes.router);
 
 // app.get('/', async (req: Request, res: Response, next: NextFunction) => {
 //   throw new Error('Errorrrrrrrrrrr.......')
